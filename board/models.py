d445fed930570efb8e68cc6309762b6a8acf90f3
Yukito SUDO <s1F101901667@iniad.org>
2020-12-27 17:32:15 +0900

    New model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Message(models.Model):
+    """チャットの履歴保存"""
+    user = models.CharField(max_length=256)
+    message = models.TextField(default='')
+    created = models.DateTimeField(default=datetime.datetime.now)
