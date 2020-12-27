8e3a97fe6aaa9c3a8f4d47b9edcbc472fc4ed0f0
Yukito SUDO <s1F101901667@iniad.org>
2020-12-27 17:58:10 +0900

    Add. Save chat message to sqlite3

--- board/consumers.py
+++ board/consumers.py
@@ -1,6 +1,7 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+from board.models import Message
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -25,6 +26,10 @@ class ChatConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             { 'type': 'chat_message', 'message': message }
+        )
+        Message.objects.create(
+            user = "名無し",
+            message = message,
         )
     # Receive message from room group
     def chat_message(self, event):
