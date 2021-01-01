517e0bf61d06cc57b6ee8c0322330f4e03287318
Yukito SUDO <s1F101901667@iniad.org>
2021-01-02 00:49:43 +0900

    Update. Models

--- board/models.py
+++ board/models.py
@@ -4,7 +4,7 @@ import datetime
 # Create your models here.
 class Message(models.Model):
     """チャットの履歴保存"""
-    course = models.CharField(max_length=256, choices=(('e', 'engineer'), ('d', 'design'), ('b', 'business'), ('c', 'civil')))
-    user = models.CharField(max_length=256)
+    course = models.CharField(max_length=8, default='engineer')
+    user = models.CharField(max_length=256, default='NO_NAME')
     message = models.TextField(default='')
     created = models.DateTimeField(default=datetime.datetime.now)
